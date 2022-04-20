bbbe2d15c26c85faf00f12c6893f550e0f715362
Gerald Wodni <gerald.wodni@gmail.com>
2022-04-20 16:34:32 +0200

    HTML env removed

--- run.sh
+++ run.sh
@@ -7,15 +7,12 @@ KERN_VIEW_DE_HEADER=Wartungsarbeiten
 export KERN_VIEW_DE_HEADER
 KERN_VIEW_EN_HEADER=Maintenance
 export KERN_VIEW_EN_HEADER
-KERN_VIEW_DE_TEXT="Wir führen dringend Notwendige Arbeiten am System durch, und möchten Sie um Geduld bitten.
 
+KERN_VIEW_DE_TEXT="Wir führen dringend notwendige Arbeiten am System durch, und möchten Sie um Geduld bitten.
 Wir sind bald wieder für Sie da."
 export KERN_VIEW_DE_TEXT
-KERN_VIEW_DE_HTML="<p><b>Wir führen dringend Notwendige Arbeiten am System durch,
-und möchten Sie um Geduld bitten.<br/>Wir sind bald wieder für Sie da.</b></p>"
-export KERN_VIEW_DE_HTML
-KERN_VIEW_EN_TEXT="We are performing mandatory maintenance on the system, and are asking for your patience.
 
+KERN_VIEW_EN_TEXT="We are performing mandatory maintenance on the system, and are asking for your patience.
 We will be back shortly."
 export KERN_VIEW_EN_TEXT
 
