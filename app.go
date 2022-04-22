e094166086c3b6d2357711b174348073fcd7596b
Gerald Wodni <gerald.wodni@gmail.com>
2022-04-23 00:27:50 +0200

    defaultHandler checks for X-Code request header

--- app.go
+++ app.go
@@ -5,6 +5,7 @@ package main
 import (
     "fmt"
     "net/http"
+    "strconv"
     "strings"
 
     "github.com/GeraldWodni/kern.go"
@@ -13,32 +14,68 @@ import (
     "github.com/GeraldWodni/kern.go/view"
 )
 
+var layoutViews []view.HtmlView
+var errorView *view.HtmlView
+var indexView *view.HtmlView
+
+func renderStatusCode(res http.ResponseWriter, req *http.Request, next router.RouteNext, statusCode StatusCode) {
+    res.Header().Set("Content-Type", errorView.ContentType )
+    code, _ := strconv.Atoi( statusCode.Code )
+    res.WriteHeader( code )
+    errorView.Render( res, req, next, struct {
+        StatusCode StatusCode
+    }{
+        StatusCode: statusCode,
+    })
+}
+
+func defaultHandler(res http.ResponseWriter, req *http.Request, next router.RouteNext) {
+    if xCode := req.Header.Get("X-Code"); xCode != "" {
+        statusCode := GetStatusCode( xCode )
+        code, _ := strconv.Atoi( statusCode.Code )
+        if xFormat := req.Header.Get("X-Format"); xFormat != "" {
+            if strings.Contains( xFormat, "text/html" ) {
+                renderStatusCode( res, req, next, statusCode )
+                return
+            } else if strings.Contains( xFormat, "application/json" ) {
+                res.Header().Set("Content-Type", "application/json")
+                res.WriteHeader( code )
+                fmt.Fprintf( res, "{\"err\":\"%s %s\"}", statusCode.Code, statusCode.MessageEN )
+                return
+            }
+        }
+        res.Header().Set("Content-Type", "text/plain")
+        res.WriteHeader( code )
+        fmt.Fprintf( res, statusCode.String() )
+        return
+    }
+
+    res.Header().Set("Content-Type", indexView.ContentType )
+    indexView.Render( res, req, next, nil )
+}
+
 func main() {
     // create new kern instance on port 5000
     app := kern.New(":5000", []string{ "content" } )
 
-    LayoutViews, _ := app.Hierarchy.LookupDirectory( "views/layout" )
+    layoutViews, _ := app.Hierarchy.LookupDirectory( "views/layout" )
 
-    errorView, err := view.NewHtml( append( LayoutViews, app.Hierarchy.LookupFatal( "views", "error.gohtml" ) )... )
+    var err error
+    errorView, err = view.NewHtml( append( layoutViews, app.Hierarchy.LookupFatal( "views", "error.gohtml" ) )... )
     if err != nil {
         log.Fatal( "Cannot load error.gohtml", err )
     }
+    indexView, err = view.NewHtml( append( layoutViews, app.Hierarchy.LookupFatal( "views", "index.gohtml" ) )... )
+
     app.Router.Get("/code/", func (res http.ResponseWriter, req *http.Request, next router.RouteNext ) {
-        var statusCode StatusCode
         code := strings.TrimPrefix( req.URL.Path, "/code/" )
-        fmt.Printf( code )
-        statusCode = GetStatusCode( code )
-        res.Header().Set("Content-Type", errorView.ContentType )
-        errorView.Render( res, req, next, struct {
-            StatusCode StatusCode
-        }{
-            StatusCode: statusCode,
-        })
+        statusCode := GetStatusCode( code )
+        renderStatusCode( res, req, next, statusCode )
     })
 
     // mount index.gohtml on "/"
     app.Router.Get("/css/theme.css", view.NewCssHandler( app.Hierarchy.LookupFatal( "css", "theme.gocss" ) ) )
-    app.Router.Get("/", view.NewHtmlHandler( append( LayoutViews, app.Hierarchy.LookupFatal( "views", "index.gohtml" ) )... ) )
+    app.Router.Get("/", defaultHandler )
 
     // start server
     app.Run()
